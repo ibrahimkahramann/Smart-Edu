750fc28cb2631cba0f802a849ab553473f4e4f67
ibrahimkahramann <ibrahimk.bg06@gmail.com>
2023-07-24 13:43:13 +0300

    authorization added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -13,4 +14,41 @@ exports.createUser = async (req, res) => {
       error,
     });
   }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user) {
+      await bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // USER SESSION
+
+          req.session.userID = user._id;
+          res.status(200).redirect('/users/dashboard');
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    user,
+    page_name: 'dashboard',
+  });
 };
